3d2e4d46e6784ccd24f975e06f0e1f66e89feea6
Kohei Kawabata <kohei.kawabata@mac.com>
2020-03-08 18:54:37 +0900

    Add spring cloud functions sample for AWS API Gateway.

--- src/main/java/org/debugroom/mynavi/sample/aws/lambda/ApiGatewayEventHandler.java
+++ src/main/java/org/debugroom/mynavi/sample/aws/lambda/ApiGatewayEventHandler.java
@@ -2,5 +2,8 @@ package org.debugroom.mynavi.sample.aws.lambda;
 
 import org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ApiGatewayEventHandler extends SpringBootApiGatewayRequestHandler {
 }
--- src/main/java/org/debugroom/mynavi/sample/aws/lambda/functions/ApiGatewayEventFunction.java
+++ src/main/java/org/debugroom/mynavi/sample/aws/lambda/functions/ApiGatewayEventFunction.java
@@ -1,16 +1,13 @@
 package org.debugroom.mynavi.sample.aws.lambda.functions;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
-
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import lombok.extern.slf4j.Slf4j;
 import org.debugroom.mynavi.sample.aws.lambda.model.Input;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-import reactor.core.publisher.Flux;
 
 @Slf4j
 public class ApiGatewayEventFunction implements Function<Message<Input>, Message<String>> {
@@ -26,7 +23,8 @@ public class ApiGatewayEventFunction implements Function<Message<Input>, Message
 
             @Override
             public MessageHeaders getHeaders() {
-                return null;
+                Map<String, Object> headers = new HashMap<>();
+                return new MessageHeaders(headers);
             }
         };
 
